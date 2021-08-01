cdebdefef15600c6b4cbbfb817f8dffa35740d3f
sincy <sincyraj86@gmail.com>
2021-08-01 20:59:48 +0200

    Initial commit

---
+++ src/main/java/com/vub/osproject/OsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vub.osproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsProjectApplication.class, args);
+	}
+
+}
